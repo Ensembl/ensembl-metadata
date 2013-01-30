e6a9263d3ca282c91377a83ecdc0c83625981623
dstaines <>
2013-01-30 13:12:48 +0000

    fixed issues introduced by reworking of code

--- modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper/TT2MetaDataDumper.pm
+++ modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper/TT2MetaDataDumper.pm
@@ -31,7 +31,7 @@ use warnings;
 sub new {
   my ($proto, @args) = @_;
   my $self = $proto->SUPER::new(@args);
-  $self->{file} ||= 'species.tt2';
+  $self->{file}     ||= 'species.tt2';
   $self->{division} ||= 0;
   $self->{div_links} = {EnsemblBacteria => "http://bacteria.ensembl.org/",
 						EnsemblFungi    => "http://fungi.ensembl.org/",
@@ -46,11 +46,37 @@ sub do_dump {
   my ($self, $metadata, $outfile) = @_;
   my $td  = '<td>';
   my $tdx = '</td>';
-  my $tr  = '<td>';
-  my $trx = '</td>';
+  my $tr  = '<tr>';
+  my $trx = '</tr>';
   open(my $tt2_file, '>', $outfile)
 	|| croak "Could not write to " . $outfile;
-  print $tt2_file join('<table><tr>', '<th>Species</th>', '<th>Division</th>', '<th>Taxonomy ID</th>', '<th>Assembly</th>', '<th>Genebuild</th>', '<th>Variation</th>', '<th>Pan compara</th>', '<th>Genome alignments</th>', '<th>Other alignments</th>', '</tr>', "\n");
+  print $tt2_file <<'ENDHEAD';
+	<style type="text/css" title="currentStyle">
+    	@import "/static/css/grid.css";
+	</style>
+
+	<script type="text/javascript" charset="utf-8" src="/static/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript">
+	$(document).ready( function () {
+   	$('#species_list').dataTable({
+    "sPaginationType": "full_numbers",
+    "iDisplayLength": 25,
+    "aLengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]]
+});
+} );
+</script> 
+<div id="static">
+<h2>Ensembl Genomes Release 17</h2>
+<table cellpadding="0" cellspacing="0" border="1px dotted #eeeeee"
+class="display" id="species_list">
+<thead> 
+  <tr>
+  <th>Species</th><th>Division</th><th>Taxonomy ID</th><th>Assembly</th><th>Genebuild</th><th>Variation</th><th>Pan compara</th><th>Genome alignments</th><th>Other alignments</th>
+  </tr>
+  </thead>
+  <tbody>
+ENDHEAD
 
   for my $md (@{$metadata->{genome}}) {
 	my $div_link = $self->{div_links}->{$md->{division}};
@@ -58,13 +84,21 @@ sub do_dump {
 	  croak "No division defined for $md->{name}";
 	}
 	$div_link .= $md->{species};
-	my $line = join($tr, $td, "<a href='${div_link}'>", $md->{name}, '</a>', $tdx, $td, "<a href='", $div_link, "'>", $md->{division}, '</a>', $td, $md->{taxonomy_id}, $tdx, $td, $md->{assembly_name}, $tdx, $td, $md->{genebuild}, $tdx, $td, $self->yesno($self->count_variation($md)), $tdx, $td, $self->yesno($md->{pan_compara}), $tdx, $td, $self->yesno($self->count_dna_compara($md)), $tdx, $td, $self->yesno($self->count_alignments($md)), $tdx, $trx, "\n");
-	print $tt2_file $line;
+	print $tt2_file join("",$tr, $td, "<a href='${div_link}'>", $md->{name}, '</a>', $tdx, $td, "<a href='", $div_link, "'>", $md->{division}, '</a>', $td, $md->{taxonomy_id}, $tdx, $td, $md->{assembly_name}, $tdx, $td, $md->{genebuild}, $tdx, $td, $self->yesno($self->count_variation($md)), $tdx, $td, $self->yesno($md->{pan_compara}), $tdx, $td, $self->yesno($self->count_dna_compara($md)), $tdx, $td, $self->yesno($self->count_alignments($md)), $tdx, $trx, "\n");
   }
-  print $tt2_file '</table></div>';
+  print $tt2_file <<'ENDFOOT';
+</tbody>
+<tfoot>
+<tr>
+  <th>Species</th><th>Division</th><th>Taxonomy ID</th><th>Assembly</th><th>Genebuild</th><th>Variation</th><th>Pan compara</th><th>Genome alignments</th><th>Other alignments</th>
+</tr>
+</tfoot>
+</table>
+</div>
+ENDFOOT
   close $tt2_file;
   return;
-}
+} ## end sub do_dump
 
 1;
 
