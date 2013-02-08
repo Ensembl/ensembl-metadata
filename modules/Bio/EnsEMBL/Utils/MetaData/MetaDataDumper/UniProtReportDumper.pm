a5530831b90e374761716d169cc4b5395e29c00b
dstaines <>
2013-02-08 12:42:12 +0000

    added genebuild and assembly names

--- modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper/UniProtReportDumper.pm
+++ modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper/UniProtReportDumper.pm
@@ -40,9 +40,14 @@ sub do_dump {
   my ($self, $metadata, $outfile) = @_;
   open(my $txt_file, '>', $outfile)
 	|| croak "Could not write to " . $outfile;
-  print $txt_file join("\t", qw(name species division taxonomy_id assembly_id nProteinCoding nProteinCodingUniProtKBSwissProt nProteinCodingUniProtKBTrEMBL uniprotCoverage)) . "\n";
+  print $txt_file join("\t", qw(name species division taxonomy_id assembly_id assembly_name genebuild nProteinCoding nProteinCodingUniProtKBSwissProt nProteinCodingUniProtKBTrEMBL uniprotCoverage)) . "\n";
   for my $md (sort { $self->get_uniprot_coverage($a) <=> $self->get_uniprot_coverage($b) } @{$metadata->{genome}}) {
-	print $txt_file join("\t", ($md->{name}, $md->{species},$md->{division}, $md->{taxonomy_id}, $md->{assembly_id}, $md->{annotation}{nProteinCoding}, $md->{annotation}{nProteinCodingUniProtKBSwissProt}, $md->{annotation}{nProteinCodingUniProtKBTrEMBL}, $self->get_uniprot_coverage($md), "\n"));
+	print $txt_file join(
+	  "\t", (
+	   $md->{name}, $md->{species}, $md->{division}, $md->{taxonomy_id}, $md->{assembly_id},
+	   $md->{assembly_name}, $md->{genebuild},  $md->{annotation}{nProteinCoding}, 
+	   $md->{annotation}{nProteinCodingUniProtKBSwissProt}, $md->{annotation}{nProteinCodingUniProtKBTrEMBL}, 
+	   $self->get_uniprot_coverage($md), "\n"));
   }
   close $txt_file;
   return;
