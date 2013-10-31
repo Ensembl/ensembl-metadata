0b0c519c0868fb810a9269888d302bf6646c6c40
dstaines <dstaines@ebi.ac.uk>
2013-10-31 10:28:06 +0000

    added strain, serotype, base count, assembly level, publications

--- modules/Bio/EnsEMBL/Utils/MetaData/MetaDataProcessor.pm
+++ modules/Bio/EnsEMBL/Utils/MetaData/MetaDataProcessor.pm
@@ -32,7 +32,11 @@ sub new {
   my ($caller, @args) = @_;
   my $class = ref($caller) || $caller;
   my $self = bless({}, $class);
-  ($self->{contigs}, $self->{annotation_analyzer}) = rearrange(['CONTIGS', 'ANNOTATION_ANALYZER', 'VARIATION', 'COMPARA'], @args);
+  ($self->{contigs}, $self->{annotation_analyzer}) =
+	rearrange(
+			  ['CONTIGS', 'ANNOTATION_ANALYZER', 'VARIATION', 'COMPARA'
+			  ],
+			  @args);
   $self->{logger} = get_logger();
   return $self;
 }
@@ -56,7 +60,8 @@ sub process_metadata {
   my $pan_species = {};
   my $pan_compara = $dba_hash->{compara}{pan_homology};
   if (defined $pan_compara) {
-	(my $mlss) = @{$pan_compara->get_MethodLinkSpeciesSetAdaptor()->fetch_all_by_method_link_type("PROTEIN_TREES")};
+	(my $mlss) = @{$pan_compara->get_MethodLinkSpeciesSetAdaptor()
+		->fetch_all_by_method_link_type("PROTEIN_TREES")};
 	for my $gdb (@{$mlss->species_set_obj->genome_dbs()}) {
 	  $pan_species->{$gdb->name()} = 1;
 	}
@@ -65,22 +70,47 @@ sub process_metadata {
   my $total = scalar(values %{$dba_hash->{core}});
   for my $dba (values %{$dba_hash->{core}}) {
 	eval {
-	  $self->{logger}->info("Processing " . $dba->species() . " (" . ++$n . "/$total)");
+	  $self->{logger}->info(
+			"Processing " . $dba->species() . " (" . ++$n . "/$total)");
 	  # get metadata container
 	  my $meta   = $dba->get_MetaContainer();
 	  my $dbname = $dba->dbc()->dbname();
-	  my $size   = $dba->dbc()->sql_helper()->execute_single_result(-SQL => "select SUM(data_length + index_length) from information_schema.tables where table_schema=?", -PARAMS => [$dbname]);
-	  my $tableN = $dba->dbc()->sql_helper()->execute_single_result(-SQL => "select count(*) from information_schema.tables where table_schema=?", -PARAMS => [$dbname]);
-	  my $type = get_type($dbname);
-	  my $md = {species       => $dba->species(),
-				species_id    => $dba->species_id(),
-				name          => $meta->get_scientific_name() || '',
-				taxonomy_id   => $meta->get_taxonomy_id() || '',
-				assembly_id   => $meta->single_value_by_key('assembly.accession') || '',
-				assembly_name => $meta->single_value_by_key('assembly.name') || '',
-				genebuild     => $meta->single_value_by_key('genebuild.start_date') || '',
-				division      => $meta->get_division() || 'Ensembl',
-				dbname=>$dbname};
+	  my $size =
+		$dba->dbc()->sql_helper()->execute_single_result(
+		-SQL =>
+"select SUM(data_length + index_length) from information_schema.tables where table_schema=?",
+		-PARAMS => [$dbname]);
+	  my $tableN =
+		$dba->dbc()->sql_helper()->execute_single_result(
+		-SQL =>
+"select count(*) from information_schema.tables where table_schema=?",
+		-PARAMS => [$dbname]);
+	  #my $type = get_type($dbname);
+	  my $md = {
+		 species    => $dba->species(),
+		 species_id => $dba->species_id(),
+		 strain => $meta->single_value_by_key('species.strain') ||
+		   '',
+		 serotype => $meta->single_value_by_key('species.serotype')
+		   || '',
+		 name        => $meta->get_scientific_name() || '',
+		 taxonomy_id => $meta->get_taxonomy_id()     || '',
+		 assembly_id => $meta->single_value_by_key('assembly.accession')
+		   || '',
+		 assembly_name => $meta->single_value_by_key('assembly.name') ||
+		   '',
+		 genebuild => $meta->single_value_by_key('genebuild.start_date')
+		   || '',
+		 division => $meta->get_division() || 'Ensembl',
+		 dbname => $dbname};
+
+	  # get highest assembly level
+	  $md->{assembly_level} = @{
+		$dba->dbc()->sql_helper()->execute_simple(
+		  -SQL =>
+'select name from coord_system where species_id=? order by rank asc',
+		  -PARAMS => [$dba->species_id()])}[0];
+	  
 	  # get list of seqlevel contigs
 	  if (defined $self->{contigs}) {
 		my $slice_adaptor = $dba->get_SliceAdaptor();
@@ -88,47 +118,88 @@ sub process_metadata {
 		  push @{$md->{accession}}, $contig->seq_region_name();
 		}
 	  }
+	  
+	  # get toplevel base count
+	  $md->{base_count} =
+		$dba->dbc()->sql_helper()->execute_single_result(
+		-SQL => q/select sum(length) 
+	 from seq_region s 
+	 join seq_region_attrib sa using (seq_region_id) 
+	 join attrib_type a using (attrib_type_id) 
+	 join coord_system cs using (coord_system_id) 
+	 where code='toplevel' and species_id=?/,
+		-PARAMS => [$dba->species_id()]);
+		
+	  # get associated PMIDs
+	  $md->{publications} = $dba->dbc()->sql_helper()->execute_simple(
+		-SQL => q/select distinct dbprimary_acc from 
+	  xref
+	  join external_db using (external_db_id)
+	  join seq_region_attrib sa on (xref.xref_id=sa.value)
+	  join attrib_type using (attrib_type_id)
+	  join seq_region using (seq_region_id)
+	  join coord_system using (coord_system_id)
+	  where species_id=? and code='xref_id' and db_name in ('PUBMED')/,
+		-PARAMS => [$dba->species_id()]);
 	  if (defined $self->{annotation_analyzer}) {
 		# core annotation
-		$self->{logger}->info("Processing " . $dba->species() . " core annotation");
-		$md->{annotation} = $self->{annotation_analyzer}->analyze_annotation($dba);
+		$self->{logger}
+		  ->info("Processing " . $dba->species() . " core annotation");
+		$md->{annotation} =
+		  $self->{annotation_analyzer}->analyze_annotation($dba);
 		# features
-		$md->{features} = $self->{annotation_analyzer}->analyze_features($dba);
-		my $other_features = $dba_hash->{otherfeatures}{$dba->species()};
+		$md->{features} =
+		  $self->{annotation_analyzer}->analyze_features($dba);
+		my $other_features =
+		  $dba_hash->{otherfeatures}{$dba->species()};
 		if (defined $other_features) {
-		  $self->{logger}->info("Processing " . $dba->species() . " otherfeatures annotation");
-		  $md->{features} = {%{$md->{features}}, %{$self->{annotation_analyzer}->analyze_features($other_features)}};
+		  $self->{logger}->info("Processing " .
+						 $dba->species() . " otherfeatures annotation");
+		  $md->{features} = {%{$md->{features}},
+							 %{$self->{annotation_analyzer}
+								 ->analyze_features($other_features)}};
 		}
 		# variation
 		my $variation = $dba_hash->{variation}{$dba->species()};
 		if (defined $variation) {
-		  $self->{logger}->info("Processing " . $dba->species() . " variation annotation");
-		  $md->{variation} = $self->{annotation_analyzer}->analyze_variation($variation);
+		  $self->{logger}->info(
+			 "Processing " . $dba->species() . " variation annotation");
+		  $md->{variation} =
+			$self->{annotation_analyzer}->analyze_variation($variation);
 		}
 		# BAM
-		$md->{bam} = $self->{annotation_analyzer}->analyze_tracks($md->{species}, $md->{division});
+		$md->{bam} = $self->{annotation_analyzer}
+		  ->analyze_tracks($md->{species}, $md->{division});
 		# compara
 		(my $compara_div = lc $md->{division}) =~ s/ensembl//;
 		my $compara = $dba_hash->{compara}{$compara_div};
 		if (defined $compara) {
-		  $self->{logger}->info("Processing " . $dba->species() . " compara annotation");
-		  $md->{compara} = $self->{annotation_analyzer}->analyze_compara($compara, $dba);
+		  $self->{logger}->info(
+			   "Processing " . $dba->species() . " compara annotation");
+		  $md->{compara} = $self->{annotation_analyzer}
+			->analyze_compara($compara, $dba);
 		}
 		if (defined $pan_species->{$dba->species()}) {
 		  $md->{pan_species} = 1;
-		} else {
+		}
+		else {
 		  $md->{pan_species} = 0;
 		}
 	  } ## end if (defined $self->{annotation_analyzer...})
 	  push @{$metadata->{genome}}, $md;
 	};
 	if ($@) {
-	  throw "Could not get metadata for species " . $dba->species() . ":" . $@;
+	  throw "Could not get metadata for species " .
+		$dba->species() . ":" . $@;
 	}
   } ## end for my $dba (values %{$dba_hash...})
 
   if (defined $metadata->{genome}) {
-	$metadata->{genome} = [sort { $a->{division} cmp $b->{division} or $a->{name} cmp $b->{name} } @{$metadata->{genome}}];
+	$metadata->{genome} = [
+	  sort {
+		$a->{division} cmp $b->{division} or
+		  $a->{name} cmp $b->{name}
+	  } @{$metadata->{genome}}];
   }
   return $metadata;
 } ## end sub process_metadata
