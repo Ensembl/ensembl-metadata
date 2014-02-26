bdf531e9443c1c5c2d0ab3ce0f34a101b8169aa1
Dan Staines <dstaines@ebi.ac.uk>
2014-02-26 16:56:16 +0000

    added license

--- sql/genome_info.sql
+++ sql/genome_info.sql
@@ -1,8 +1,16 @@
--- MySQL dump 10.14  Distrib 5.5.34-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: genome_info
--- ------------------------------------------------------
--- Server version	5.5.34-MariaDB
+-- Copyright [1999-2014] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
