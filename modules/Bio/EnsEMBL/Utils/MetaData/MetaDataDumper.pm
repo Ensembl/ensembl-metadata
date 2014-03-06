9356c9afcd7dce9914df917b81a9a3cca04ed7cf
Dan Staines <dstaines@ebi.ac.uk>
2014-03-06 15:13:07 +0000

    REPORTED - issue EG-1864: improve LookUp performance https://www.ebi.ac.uk/panda/jira/browse/EG-1864 revised to work with new objects

--- modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper.pm
+++ modules/Bio/EnsEMBL/Utils/MetaData/MetaDataDumper.pm
@@ -64,8 +64,8 @@ sub dump_metadata {
   if (defined $self->division() && $self->division() == 1) {
 
 	my %mds_per_division;
-	for my $md (@{$metadata->{genome}}) {
-	  push @{$mds_per_division{$md->{division}}{genome}}, $md;
+	for my $md (@{$metadata}) {
+	  push @{$mds_per_division{$md->division()}}, $md;
 	}
 	for my $division (keys %mds_per_division) {
 	  (my $out_file = $self->file()) =~
@@ -98,6 +98,15 @@ sub logger {
 sub yesno {
   my ($self, $num) = @_;
   return (defined $num && $num + 0 > 0) ? 'Y' : 'N';
+}
+
+sub metadata_to_hash {
+	my ($self, $metadata) = @_;
+	my $genomes = [];
+	for my $md (@{$metadata}) {
+		push @$genomes, $md->to_hash(1);
+	}
+	return {genome=>$genomes};	
 }
 
 1;
