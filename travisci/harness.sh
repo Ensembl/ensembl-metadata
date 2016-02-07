22973ef527fe5c85067fcc29693967671e505c09
Dan Staines <dstaines@ebi.ac.uk>
2016-02-07 12:25:02 +0000

    travis support

---
+++ travisci/harness.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-taxonomy:$PWD/modules
+
+SKIP_TESTS="--skip 55-build_pruned_tree_big.t"
+if [ "$DB" = 'mysql' ]; then
+    (cd modules/t && ln -sf MultiTestDB.conf.mysql MultiTestDB.conf)
+elif [ "$DB" = 'sqlite' ]; then
+    (cd modules/t && ln -sf MultiTestDB.conf.SQLite MultiTestDB.conf)
+else
+    echo "Don't know about DB '$DB'"
+    exit 1;
+fi
+
+echo "Running test suite"
+if [ "$COVERALLS" = 'true' ]; then
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose modules/t $SKIP_TESTS
+else
+  perl $PWD/ensembl-test/scripts/runtests.pl modules/t $SKIP_TESTS
+fi
+
+rt=$?
+if [ $rt -eq 0 ]; then
+  if [ "$COVERALLS" = 'true' ]; then
+    echo "Running Devel::Cover coveralls report"
+    cover --nosummary -report coveralls
+  fi
+  exit $?
+else
+  exit $rt
+fi
