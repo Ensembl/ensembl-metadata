92288cfbb89d9dd1d523adc0f4eaf6ff39f0ceb4
Dan Staines <dstaines@ebi.ac.uk>
2016-03-04 14:19:39 +0000

    do not store assembly seqs when updating as this is very expensive

--- modules/Bio/EnsEMBL/MetaData/DBSQL/GenomeAssemblyInfoAdaptor.pm
+++ modules/Bio/EnsEMBL/MetaData/DBSQL/GenomeAssemblyInfoAdaptor.pm
@@ -55,68 +55,68 @@ use List::MoreUtils qw(natatime);
 =cut
 
 sub store {
-	my ( $self, $assembly ) = @_;
-	if ( !defined $assembly->organism() ) {
-		throw("Assembly must be associated with an organism");
-	}
-	if ( !defined $assembly->organism()->dbID() ) {
-		$self->db()->get_GenomeOrganismInfoAdaptor()
-		  ->store( $assembly->organism() );
-	}
-	if ( !defined $assembly->dbID() ) {
-		# find out if organism exists first
-		my ($dbID) =
-		  @{$self->dbc()->sql_helper()->execute_simple(
-				-SQL =>
+  my ( $self, $assembly ) = @_;
+  if ( !defined $assembly->organism() ) {
+    throw("Assembly must be associated with an organism");
+  }
+  if ( !defined $assembly->organism()->dbID() ) {
+    $self->db()->get_GenomeOrganismInfoAdaptor()
+      ->store( $assembly->organism() );
+  }
+  if ( !defined $assembly->dbID() ) {
+    # find out if organism exists first
+    my ($dbID) =
+      @{
+      $self->dbc()->sql_helper()->execute_simple(
+        -SQL =>
 "select assembly_id from assembly where organism_id=? and assembly_name=?",
-				-PARAMS => [ $assembly->organism()->dbID(), $assembly->assembly_name() ]
-			) };
-		if ( defined $dbID ) {
-			$assembly->dbID($dbID);
-			$assembly->adaptor($self);
-		}
-	}
-	if ( defined $assembly->dbID() ) {
-		$self->update($assembly);
-	}
-	else {
-		$self->dbc()->sql_helper()->execute_update(
-			-SQL =>
+        -PARAMS =>
+          [ $assembly->organism()->dbID(), $assembly->assembly_name() ]
+      ) };
+    if ( defined $dbID ) {
+      $assembly->dbID($dbID);
+      $assembly->adaptor($self);
+    }
+  }
+  if ( defined $assembly->dbID() ) {
+    $self->update($assembly);
+  }
+  else {
+    $self->dbc()->sql_helper()->execute_update(
+      -SQL =>
 q/insert into assembly(assembly_accession,assembly_name,assembly_level,base_count,organism_id)
 		values(?,?,?,?,?)/,
-			-PARAMS => [ $assembly->assembly_accession(),
-						 $assembly->assembly_name(),
-						 $assembly->assembly_level(),
-						 $assembly->base_count(),
-						 $assembly->organism()->dbID() ],
-			-CALLBACK => sub {
-				my ( $sth, $dbh, $rv ) = @_;
-				$assembly->dbID( $dbh->{mysql_insertid} );
-			} );
-		$self->_store_sequences($assembly);
-		$assembly->adaptor($self);
-		$self->_store_cached_obj($assembly);
-	}
-	return;
+      -PARAMS => [ $assembly->assembly_accession(),
+                   $assembly->assembly_name(),
+                   $assembly->assembly_level(),
+                   $assembly->base_count(),
+                   $assembly->organism()->dbID() ],
+      -CALLBACK => sub {
+        my ( $sth, $dbh, $rv ) = @_;
+        $assembly->dbID( $dbh->{mysql_insertid} );
+      } );
+    $self->_store_sequences($assembly);
+    $assembly->adaptor($self);
+    $self->_store_cached_obj($assembly);
+  }
+  return;
 } ## end sub store
 
 sub update {
-	my ( $self, $assembly ) = @_;
-	if ( !defined $assembly->dbID() ) {
-		croak "Cannot update an object that has not already been stored";
-	}
+  my ( $self, $assembly ) = @_;
+  if ( !defined $assembly->dbID() ) {
+    croak "Cannot update an object that has not already been stored";
+  }
 
-	$self->dbc()->sql_helper()->execute_update(
-		-SQL =>
+  $self->dbc()->sql_helper()->execute_update(
+    -SQL =>
 q/update assembly set assembly_accession=?,assembly_name=?,assembly_level=?,base_count=?,organism_id=? where assembly_id=?/,
-		-PARAMS => [
-			$assembly->assembly_accession(),
-			$assembly->assembly_name(),
-			$assembly->assembly_level(), $assembly->base_count(),
-			$assembly->organism()->dbID(), $assembly->dbID() ] );
+    -PARAMS => [
+            $assembly->assembly_accession(), $assembly->assembly_name(),
+            $assembly->assembly_level(),     $assembly->base_count(),
+            $assembly->organism()->dbID(),   $assembly->dbID() ] );
 
-	$self->_store_sequences($assembly);
-	return;
+  return;
 }
 
 =head2 _store_sequences
@@ -129,27 +129,27 @@ q/update assembly set assembly_accession=?,assembly_name=?,assembly_level=?,base
 =cut
 
 sub _store_sequences {
-	my ( $self, $assembly ) = @_;
+  my ( $self, $assembly ) = @_;
 
-	$self->{dbc}->sql_helper()->execute_update(
-					 -SQL => q/delete from assembly_sequence where assembly_id=?/,
-					 -PARAMS => [ $assembly->dbID() ] );
+  $self->{dbc}->sql_helper()->execute_update(
+           -SQL => q/delete from assembly_sequence where assembly_id=?/,
+           -PARAMS => [ $assembly->dbID() ] );
 
-	return if !defined $assembly->sequences();
+  return if !defined $assembly->sequences();
 
-	my $it = natatime 1000, @{ $assembly->sequences() };
-	while ( my @vals = $it->() ) {
-		my $sql =
-		  'insert ignore into assembly_sequence(assembly_id,name,acc) values ' .
-		  join(
-			',',
-			map {
-				'(' . $assembly->dbID() . ',"' . $_->{name} . '",' .
-				  ( $_->{acc} ? ( '"' . $_->{acc} . '"' ) : ('NULL') ) . ')'
-			} @vals );
-		$self->dbc()->sql_helper()->execute_update( -SQL => $sql );
-	}
-	return;
+  my $it = natatime 1000, @{ $assembly->sequences() };
+  while ( my @vals = $it->() ) {
+    my $sql =
+'insert ignore into assembly_sequence(assembly_id,name,acc) values '
+      . join(
+      ',',
+      map {
+        '(' . $assembly->dbID() . ',"' . $_->{name} . '",' .
+          ( $_->{acc} ? ( '"' . $_->{acc} . '"' ) : ('NULL') ) . ')'
+        } @vals );
+    $self->dbc()->sql_helper()->execute_update( -SQL => $sql );
+  }
+  return;
 }
 
 =head2 fetch_all_by_sequence_accession
@@ -163,13 +163,15 @@ sub _store_sequences {
 =cut
 
 sub fetch_all_by_sequence_accession {
-	my ( $self, $id, $keen ) = @_;
-	if ( $id =~ m/\.[0-9]+$/ ) {
-		return $self->fetch_all_by_sequence_accession_versioned( $id, $keen );
-	}
-	else {
-		return $self->fetch_all_by_sequence_accession_unversioned( $id, $keen );
-	}
+  my ( $self, $id, $keen ) = @_;
+  if ( $id =~ m/\.[0-9]+$/ ) {
+    return $self->fetch_all_by_sequence_accession_versioned( $id,
+                                                             $keen );
+  }
+  else {
+    return $self->fetch_all_by_sequence_accession_unversioned( $id,
+                                                               $keen );
+  }
 }
 
 =head2 fetch_all_by_sequence_accession_unversioned
@@ -183,13 +185,13 @@ sub fetch_all_by_sequence_accession {
 =cut
 
 sub fetch_all_by_sequence_accession_unversioned {
-	my ( $self, $id, $keen ) = @_;
-	return
-	  $self->_fetch_generic(
-		$self->_get_base_sql() .
+  my ( $self, $id, $keen ) = @_;
+  return
+    $self->_fetch_generic(
+    $self->_get_base_sql() .
 ' where assembly_id in (select distinct(assembly_id) from assembly_sequence where acc like ? or name like ?)',
-		[ $id . '.%', $id . '.%' ],
-		$keen );
+    [ $id . '.%', $id . '.%' ],
+    $keen );
 }
 
 =head2 fetch_all_by_sequence_accession_versioned
@@ -203,13 +205,13 @@ sub fetch_all_by_sequence_accession_unversioned {
 =cut
 
 sub fetch_all_by_sequence_accession_versioned {
-	my ( $self, $id, $keen ) = @_;
-	return
-	  $self->_fetch_generic(
-		$self->_get_base_sql() .
+  my ( $self, $id, $keen ) = @_;
+  return
+    $self->_fetch_generic(
+    $self->_get_base_sql() .
 ' where assembly_id in (select distinct(assembly_id) from assembly_sequence where acc=? or name=?)',
-		[ $id, $id ],
-		$keen );
+    [ $id, $id ],
+    $keen );
 }
 
 =head2 fetch_by_assembly_accession
@@ -223,11 +225,12 @@ sub fetch_all_by_sequence_accession_versioned {
 =cut
 
 sub fetch_by_assembly_accession {
-	my ( $self, $id, $keen ) = @_;
-	return
-	  $self->_first_element( $self->_fetch_generic(
-							 $self->_get_base_sql . ' where assembly_accession=?',
-							 [ $id ], $keen ) );
+  my ( $self, $id, $keen ) = @_;
+  return
+    $self->_first_element(
+          $self->_fetch_generic(
+            $self->_get_base_sql . ' where assembly_accession=?', [$id],
+            $keen ) );
 
 }
 
@@ -242,10 +245,11 @@ sub fetch_by_assembly_accession {
 =cut
 
 sub fetch_all_by_assembly_set_chain {
-	my ( $self, $id, $keen ) = @_;
-	return $self->_fetch_generic(
-							 $self->_get_base_sql . ' where assembly_accession like ?',
-							 [ $id . '.%' ], $keen );
+  my ( $self, $id, $keen ) = @_;
+  return
+    $self->_fetch_generic(
+              $self->_get_base_sql . ' where assembly_accession like ?',
+              [ $id . '.%' ], $keen );
 }
 
 =head2 fetch_all_by_organism
@@ -259,14 +263,16 @@ sub fetch_all_by_assembly_set_chain {
 =cut
 
 sub fetch_all_by_organism {
-	my ( $self, $organism_id, $keen ) = @_;
-	if (ref($organism_id) eq 'Bio::EnsEMBL::MetaData::GenomeOrganismInfo')
-	{
-		$organism_id = $organism_id->dbID();
-	}
-	return
-	  $self->_fetch_generic( $self->_get_base_sql() . ' where organism_id = ?',
-							 [$organism_id], $keen );
+  my ( $self, $organism_id, $keen ) = @_;
+  if (
+     ref($organism_id) eq 'Bio::EnsEMBL::MetaData::GenomeOrganismInfo' )
+  {
+    $organism_id = $organism_id->dbID();
+  }
+  return
+    $self->_fetch_generic(
+                      $self->_get_base_sql() . ' where organism_id = ?',
+                      [$organism_id], $keen );
 }
 
 =head2 _fetch_sequences
@@ -279,17 +285,18 @@ sub fetch_all_by_organism {
 =cut
 
 sub _fetch_sequences {
-	my ( $self, $genome ) = @_;
-	croak
+  my ( $self, $genome ) = @_;
+  croak
 "Cannot fetch sequences for a GenomeAssemblyInfo object that has not been stored"
-	  if !defined $genome->dbID();
-	my $sequences =
-	  $self->dbc()->sql_helper()->execute(
-		   -USE_HASHREFS => 1,
-		   -SQL => 'select name,acc from assembly_sequence where assembly_id=?',
-		   -PARAMS => [ $genome->dbID() ] );
-	$genome->sequences($sequences);
-	return;
+    if !defined $genome->dbID();
+  my $sequences =
+    $self->dbc()->sql_helper()->execute(
+         -USE_HASHREFS => 1,
+         -SQL =>
+           'select name,acc from assembly_sequence where assembly_id=?',
+         -PARAMS => [ $genome->dbID() ] );
+  $genome->sequences($sequences);
+  return;
 }
 
 =head2 _fetch_children
@@ -302,28 +309,28 @@ sub _fetch_sequences {
 =cut
 
 sub _fetch_children {
-	my ( $self, $md ) = @_;
-	$self->_fetch_sequences($md);
-	if ( defined $md->{organism_id} ) {
-		$md->organism( $self->db()->get_GenomeOrganismInfoAdaptor()
-					   ->fetch_by_dbID( $md->{organism_id} ) );
-	}
-	return;
+  my ( $self, $md ) = @_;
+  $self->_fetch_sequences($md);
+  if ( defined $md->{organism_id} ) {
+    $md->organism( $self->db()->get_GenomeOrganismInfoAdaptor()
+                   ->fetch_by_dbID( $md->{organism_id} ) );
+  }
+  return;
 }
 
 my $base_organism_fetch_sql =
 q/select assembly_id as dbID, organism_id, assembly_accession, assembly_name, assembly_level, base_count from assembly/;
 
 sub _get_base_sql {
-	return $base_organism_fetch_sql;
+  return $base_organism_fetch_sql;
 }
 
 sub _get_id_field {
-	return 'assembly_id';
+  return 'assembly_id';
 }
 
 sub _get_obj_class {
-	return 'Bio::EnsEMBL::MetaData::GenomeAssemblyInfo';
+  return 'Bio::EnsEMBL::MetaData::GenomeAssemblyInfo';
 }
 
 1;
