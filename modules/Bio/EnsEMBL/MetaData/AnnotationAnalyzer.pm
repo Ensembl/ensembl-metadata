bb0e08d4ab4e130fdead01d2997ddcb07ff5f8b5
Thomas Maurel <maurel@ebi.ac.uk>
2018-02-13 16:17:27 +0000

    Number of GO and Protein coding GO was now 0 because we moved GO xrefs from Translation to Transcript. Updated both count_by_xref and count_xrefs sub to check xref level, then expended count_by_xref to be able to get a count on gene, transcript and translation level.

--- modules/Bio/EnsEMBL/MetaData/AnnotationAnalyzer.pm
+++ modules/Bio/EnsEMBL/MetaData/AnnotationAnalyzer.pm
@@ -99,18 +99,18 @@ sub analyze_annotation {
   $self->{logger}->debug( "Analysing annotation for " . $dba->species() );
   my $ann = {
       nProteinCoding => $self->count_by_biotype( $dba, 'protein_coding' ),
-      nProteinCodingGO => $self->count_by_xref( $dba, 'GO', 'protein_coding' ),
+      nProteinCodingGO => $self->count_by_xref( $dba, ['GO'], 'protein_coding' ),
       nProteinCodingUniProtKB =>
         $self->count_by_xref( $dba, [ 'Uniprot/SWISSPROT', 'Uniprot/SPTREMBL' ],
                               'protein_coding' ),
       nProteinCodingUniProtKBSwissProt =>
-        $self->count_by_xref( $dba, 'Uniprot/SWISSPROT', 'protein_coding' ),
+        $self->count_by_xref( $dba, ['Uniprot/SWISSPROT'], 'protein_coding' ),
       nProteinCodingUniProtKBTrEMBL =>
-        $self->count_by_xref( $dba, 'Uniprot/SPTREMBL', 'protein_coding' ),
+        $self->count_by_xref( $dba, ['Uniprot/SPTREMBL'], 'protein_coding' ),
       nProteinCodingInterPro => $self->count_by_interpro($dba),
-      nGO => $self->count_by_xref( $dba, 'GO', 'protein_coding' ),
-      nUniProtKBSwissProt => $self->count_xrefs( $dba, 'Uniprot/SWISSPROT' ),
-      nUniProtKBTrEMBL    => $self->count_xrefs( $dba, 'Uniprot/SPTREMBL' ),
+      nGO => $self->count_by_xref( $dba, ['GO']),
+      nUniProtKBSwissProt => $self->count_xrefs( $dba, ['Uniprot/SWISSPROT'] ),
+      nUniProtKBTrEMBL    => $self->count_xrefs( $dba, ['Uniprot/SPTREMBL'] ),
       nInterPro           => $self->count_interpro($dba),
       nInterProDomains => $self->count_interpro_domains($dba) };
   # add metadata
@@ -304,36 +304,77 @@ sub count_by_biotype {
   Status     : Stable
 =cut
 
-my $xref_gene_sql = q/select count(distinct(gene_id)) 
+my $xref_gene_sql = q/select count(distinct(gene_id))
 from gene g
-join seq_region s using (seq_region_id) 
-join coord_system c using (coord_system_id) 
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
+join transcript tr using (gene_id)
+join translation t using (transcript_id)
+join object_xref ox on (ox.ensembl_id=g.gene_id and ox.ensembl_object_type='Gene')
+join xref x using (xref_id)
+join external_db d using (external_db_id)
+where species_id=? and d.db_name=?/;
+
+my $xref_transcript_sql = q/select count(distinct(gene_id))
+from gene g
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
+join transcript tr using (gene_id)
+join translation t using (transcript_id)
+join object_xref ox on (ox.ensembl_id=tr.transcript_id and ox.ensembl_object_type='Transcript')
+join xref x using (xref_id)
+join external_db d using (external_db_id)
+where species_id=? and d.db_name=?/;
+
+my $xref_translation_sql = q/select count(distinct(gene_id))
+from gene g
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
 join transcript tr using (gene_id)
 join translation t using (transcript_id)
 join object_xref ox on (ox.ensembl_id=t.translation_id and ox.ensembl_object_type='Translation')
 join xref x using (xref_id)
 join external_db d using (external_db_id)
-where species_id=? and d.db_name in (NAMES)/;
+where species_id=? and d.db_name=?/;
+
+my $xref_level_sql = q/select distinct object_xref.ensembl_object_type
+from object_xref
+join xref on (object_xref.xref_id=xref.xref_id)
+join .external_db on (xref.external_db_id=external_db.external_db_id)
+where external_db.db_name=?/;
 
 my $biotype_clause = q/ and g.biotype=?/;
 
 sub count_by_xref {
   my ( $self, $dba, $db_names, $biotype ) = @_;
-  $self->{logger}->debug( "Counting genes by " .
-                      join( ",", $db_names ) . " xref for " . $dba->species() );
-  $db_names = [$db_names] if ( ref($db_names) ne 'ARRAY' );
-  my $sql = $xref_gene_sql;
-  my $db_name = join ',', map { "\"$_\"" } @$db_names;
-  $sql =~ s/NAMES/$db_name/;
-  my $params = [ $dba->species_id() ];
-  if ( defined $biotype ) {
-    $sql .= $biotype_clause;
-    push @$params, $biotype;
+  my $tot  = 0;
+  foreach my $db_name (@$db_names){
+    my $xref_level = $dba->dbc()->sql_helper()
+      ->execute_single_result( -SQL => $xref_level_sql, -PARAMS => [$db_name] );
+    $self->{logger}->debug( "Counting genes by " .
+                        $db_name . " xref on ".$xref_level." for " . $dba->species() );
+    my $sql;
+    if ($xref_level eq "Gene")
+    {
+      $sql=$xref_gene_sql;
+    }
+    elsif ($xref_level eq "Transcript"){
+      $sql=$xref_transcript_sql;
+    }
+    elsif ($xref_level eq "Translation"){
+      $sql=$xref_translation_sql;
+    }
+    my $params = [ $dba->species_id(), $db_name ];
+    if ( defined $biotype ) {
+      $sql .= $biotype_clause;
+      push @$params, $biotype;
+    }
+    $self->{logger}
+      ->debug( "Executing $sql with params: [" . join( ",", @$params ) );
+    $tot += $dba->dbc()->sql_helper()
+      ->execute_single_result( -SQL => $sql, -PARAMS => $params );
   }
-  $self->{logger}
-    ->debug( "Executing $sql with params: [" . join( ",", @$params ) );
-  return $dba->dbc()->sql_helper()
-    ->execute_single_result( -SQL => $sql, -PARAMS => $params );
+  return $tot;
 }
 
 =head2 count_xrefs
@@ -347,56 +388,64 @@ sub count_by_xref {
 =cut
 
 my $gene_xref_count_sql = q/
-select count(distinct(dbprimary_acc)) 
+select count(distinct(dbprimary_acc))
 from gene g
-join seq_region s using (seq_region_id) 
-join coord_system c using (coord_system_id) 
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
 join object_xref ox on (ox.ensembl_id=g.gene_id and ox.ensembl_object_type='Gene')
 join xref x using (xref_id)
 join external_db d using (external_db_id)
 where species_id=? and d.db_name=?/;
 
 my $transcript_xref_count_sql = q/
-select count(distinct(dbprimary_acc)) 
+select count(distinct(dbprimary_acc))
 from transcript tr
-join seq_region s using (seq_region_id) 
-join coord_system c using (coord_system_id) 
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
 join object_xref ox on (ox.ensembl_id=tr.transcript_id and ox.ensembl_object_type='Transcript')
 join xref x using (xref_id)
 join external_db d using (external_db_id)
 where species_id=? and d.db_name=?/;
 
 my $translation_xref_count_sql = q/
-select count(distinct(dbprimary_acc)) 
+select count(distinct(dbprimary_acc))
 from transcript tr
 join translation t using (transcript_id)
-join seq_region s using (seq_region_id) 
-join coord_system c using (coord_system_id) 
+join seq_region s using (seq_region_id)
+join coord_system c using (coord_system_id)
 join object_xref ox on (ox.ensembl_id=t.translation_id and ox.ensembl_object_type='Translation')
 join xref x using (xref_id)
 join external_db d using (external_db_id)
 where species_id=? and d.db_name=?/;
 
 sub count_xrefs {
-  my $self = shift;
-  my $dba  = shift;
+  my ($self,$dba,$db_names) = @_;
   my $tot  = 0;
-  for my $db_name (@_) {
-    $tot +=
+  for my $db_name (@$db_names) {
+    my $xref_level = $dba->dbc()->sql_helper()
+      ->execute_single_result( -SQL => $xref_level_sql, -PARAMS => [$db_name] );
+    if ($xref_level eq "Gene")
+    {
+      $tot +=
       $dba->dbc()->sql_helper()->execute_single_result(
                                      -SQL    => $gene_xref_count_sql,
                                      -PARAMS => [ $dba->species_id(), $db_name ]
       );
-    $tot +=
+    }
+    elsif ($xref_level eq "Transcript"){
+      $tot +=
       $dba->dbc()->sql_helper()->execute_single_result(
                                      -SQL    => $transcript_xref_count_sql,
                                      -PARAMS => [ $dba->species_id(), $db_name ]
       );
-    $tot +=
+    }
+    elsif ($xref_level eq "Translation"){
+      $tot +=
       $dba->dbc()->sql_helper()->execute_single_result(
                                      -SQL    => $translation_xref_count_sql,
                                      -PARAMS => [ $dba->species_id(), $db_name ]
       );
+    }
   }
   return $tot;
 }
