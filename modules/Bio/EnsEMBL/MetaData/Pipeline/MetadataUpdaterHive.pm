a3837647eed32c71975223d7b00e27163f72a430
Thomas Maurel <maurel@ebi.ac.uk>
2018-02-14 15:34:45 +0000

    Very simple hive pipeline running MetadataUpdater, Seed a database with various requiered information and the pipeline will add it to the metadata database

---
+++ modules/Bio/EnsEMBL/MetaData/Pipeline/MetadataUpdaterHive.pm
@@ -0,0 +1,55 @@
+=head1 LICENSE
+
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+  Bio::EnsEMBL::MetaData::Pipeline::MetadataUpdaterHive;
+
+=head1 DESCRIPTION
+
+=head1 MAINTAINER
+
+ maurel@ebi.ac.uk 
+
+=cut
+package Bio::EnsEMBL::MetaData::Pipeline::MetadataUpdaterHive;
+
+use base ('Bio::EnsEMBL::Hive::Process');
+use strict;
+use warnings;
+use Bio::EnsEMBL::MetaData::MetadataUpdater
+  qw/process_database/;
+
+sub run{
+my $self = shift @_;
+my $metadata_uri = $self->param_required('metadata_uri');
+my $database_uri = $self->param_required('database_uri');
+my $species = $self->param('species');
+my $db_type = $self->param('db_type');
+my $release_date = $self->param('release_date');
+my $e_release = $self->param('e_release');
+my $eg_release = $self->param('eg_release');
+my $current_release = $self->param('current_release');
+my $hive_dbc = $self->dbc;
+
+$hive_dbc->disconnect_if_idle() if defined $hive_dbc;
+
+process_database($metadata_uri,$database_uri,$species,$db_type,$release_date,$e_release,$eg_release,$current_release);
+
+return;
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/MetaData/Pipeline/MetadataUpdater_conf.pm
@@ -0,0 +1,80 @@
+=pod 
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION  
+
+=head1 LICENSE
+    Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [2016-2018] EMBL-European Bioinformatics Institute
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+         http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+=head1 CONTACT
+    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+=cut
+
+
+package Bio::EnsEMBL::MetaData::Pipeline::MetadataUpdater_conf;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');  # All Hive databases configuration files should inherit from HiveGeneric, directly or indirectly
+
+sub resource_classes {
+    my ($self) = @_;
+    return { 'default' => { 'LSF' => '-q production-rh7' }};
+}
+
+sub default_options {
+  my ($self) = @_;
+  return {
+    %{ $self->SUPER::default_options()
+      },    # inherit other stuff from the base class
+
+    pipeline_name => 'metadata_updater',
+    metadata_uri => '',
+    database_uri => '',
+    species       => '',
+    db_type => '',
+    release_date=> '',
+    e_release => '',
+    eg_release=> '',
+    current_release=>''};
+}
+
+sub pipeline_create_commands {
+    my ($self) = @_;
+    return [
+    @{$self->SUPER::pipeline_create_commands},  # inheriting database and hive tables' creation
+
+            # additional tables needed for long multiplication pipeline's operation:
+    $self->db_cmd('ALTER TABLE job DROP KEY input_id_stacks_analysis'),
+    $self->db_cmd('ALTER TABLE job MODIFY input_id TEXT')
+    ];
+  }
+
+
+=head2 pipeline_analyses
+=cut
+
+sub pipeline_analyses {
+    my ($self) = @_;
+    return [
+        {   
+            -logic_name => 'metadata_updater',
+            -module     => 'Bio::EnsEMBL::MetaData::Pipeline::MetadataUpdaterHive',
+            -input_ids => [],
+            -max_retry_count => 0,
+            -parameters => {
+             },
+            -rc_name => 'default',
+        }
+        ];
+}
+1;
